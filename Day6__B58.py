9750a714f3a1fa3ae9b2412367ec7a0352eccc27
Syedanaiyla <103213797+Syedanaiyla@users.noreply.github.com>
2022-05-25 17:01:54 +0530

    Add files via upload

---
+++ Day6__B58.py
@@ -0,0 +1,344 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+organising the list datatype
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+products = ["maybeline","swissbeauty","sugar","nyka","olay","aromabeauty"]
+
+
+# In[2]:
+
+
+print(products)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# wanted to arrange all the products in alphabetical order
+
+
+# In[3]:
+
+
+print(sorted(products)) # temporary changes in list 
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+print(products)
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+products.sort()
+
+
+# In[6]:
+
+
+print(products) # the change in the list is permanent
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+print(products)
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+# req : want the list in reverse order
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+products.reverse()
+
+
+# In[10]:
+
+
+print(products)
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+# countin the nu,ber of elements in a list
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+len(products)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+## slicing
+
+
+# In[16]:
+
+
+students = ["naiyla","kiran","raju","himanshi","rahul","arjun","john","keerti"]
+
+
+# In[17]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[18]:
+
+
+type(students)
+
+
+# In[38]:
+
+
+print(students[0:8])
+
+
+# In[19]:
+
+
+print(students[0:3])
+
+
+# In[20]:
+
+
+print(students[3:5])
+
+
+# In[21]:
+
+
+print(students[6:8])
+
+
+# In[23]:
+
+
+print(students[2:5])
+
+
+# In[24]:
+
+
+print(students[0:7:2])
+
+
+# In[25]:
+
+
+print(students[0:7:3])
+
+
+# In[41]:
+
+
+print(students[0:7:4])
+
+
+# In[ ]:
+
+
+
+
+
+# In[31]:
+
+
+fruits = ["apple","banaba","grapes","kiwi","orange","jackfruits","watermelon","pomegranet","custadapple","guava","blackgrapes","pineapple"]
+
+
+# In[32]:
+
+
+print(fruits)
+
+
+# In[36]:
+
+
+print(fruits[0:11:2])
+
+
+# In[37]:
+
+
+print(fruits[0:11:3])
+
+
+# In[42]:
+
+
+print(fruits[0:11:5])
+
+
+# In[43]:
+
+
+print(fruits[0:11:8])
+
+
+# In[44]:
+
+
+print(fruits[1:11:3])
+
+
+# In[45]:
+
+
+print(products)
+
+
+# In[47]:
+
+
+products.reverse()
+
+
+# In[48]:
+
+
+print(products)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
